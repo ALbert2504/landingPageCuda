3ef393b1bbc76e633de7d7d992d82afca4e0ef96
Aliko <61713118+ALbert2504@users.noreply.github.com>
2020-05-04 20:19:03 +0400

    last commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# landingPage (Cuda)
+UGeek
+
+A simple landing page Cuda
+psd file download link:
+https://graphicburger.com/cuda-single-page-portfolio-template/
---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+function transform(x) {
+    x.classList.toggle("change");
+    let menu = document.getElementById("dropdown__menu");
+    menu.classList.toggle("appear");
+}
